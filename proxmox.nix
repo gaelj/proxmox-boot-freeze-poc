693fc0d369a2f020d0f9d59f97deaa60bc6de7ca
Gaël James <gaeljames@gmail.com>
2026-08-04 18:09:38 +0200

    Add substituter

--- proxmox.nix
+++ proxmox.nix
@@ -10,5 +10,13 @@
       enable = true;
       ipAddress = "192.168.0.1";
     };
+    nix.settings = {
+      substituters = [
+        https://cache.saumon.network/proxmox-nixos
+      ];
+      trusted-public-keys = [
+        proxmox-nixos:D9RYSWpQQC/msZUWphOY2I5RLH5Dd6yQcaHIuug7dWM=
+      ];
+    };
   };
 }
